82771fd7604e6578147864becd5e25c608aee789
SeifRashedd <seifrashedswe@gmail.com>
2024-04-28 17:32:47 +0300

    create first iteam

--- lib/Features/home/pressentaion/views/widgets/home_view_body.dart
+++ lib/Features/home/pressentaion/views/widgets/home_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:booklyapp/Features/home/pressentaion/views/widgets/custom_app_bar.dart';
+import 'package:booklyapp/Features/home/pressentaion/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -7,7 +9,10 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppBar()],
+      children: [
+        CustomAppBar(),
+        CustomListViewItem(),
+      ],
     );
   }
 }
