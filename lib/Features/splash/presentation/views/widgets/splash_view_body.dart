e4b5ef302887144dacedc8c20cc9d820879195fa
SeifRashedd <seifrashedswe@gmail.com>
2024-04-28 15:06:44 +0300

    add animation

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,31 @@
 import 'package:booklyapp/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation = Tween(begin: const Offset(0, 2), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+    slidingAnimation.addListener(() {
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +35,12 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Image.asset(AssetsData.logo),
         const Gap(4),
-        const Text(
-          'Read free books',
-          textAlign: TextAlign.center,
+        SlideTransition(
+          position: slidingAnimation,
+          child: const Text(
+            'Read free books',
+            textAlign: TextAlign.center,
+          ),
         ),
       ],
     );
