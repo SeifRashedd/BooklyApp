e1cf60ce4ae076a3943cdd824be34e74c48674d5
SeifRashedd <seifrashedswe@gmail.com>
2024-04-29 12:18:54 +0300

    add featured list view iteam

--- lib/Features/home/pressentaion/views/widgets/custom_list_view_item.dart
+++ lib/Features/home/pressentaion/views/widgets/custom_list_view_item.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
 import '../../../../../core/utils/assets.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.5 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.red,
-            image: const DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(AssetsData.testimage),
-            ),
+    return AspectRatio(
+      aspectRatio: 2.5 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.red,
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(AssetsData.testimage),
           ),
         ),
       ),
