08e64229ba47e5de990af737f4cf508591122db2
SeifRashedd <seifrashedswe@gmail.com>
2024-04-29 12:35:42 +0300

    creat styles  and add new text

---
+++ lib/Features/home/pressentaion/views/widgets/featured_books_list_view.dart
@@ -0,0 +1,26 @@
+import 'package:booklyapp/Features/home/pressentaion/views/widgets/custom_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 6),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * .3,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: FeaturedListViewItem(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/Features/home/pressentaion/views/widgets/home_view_body.dart
+++ lib/Features/home/pressentaion/views/widgets/home_view_body.dart
@@ -1,39 +1,29 @@
 import 'package:booklyapp/Features/home/pressentaion/views/widgets/custom_app_bar.dart';
-import 'package:booklyapp/Features/home/pressentaion/views/widgets/custom_list_view_item.dart';
+import 'package:booklyapp/Features/home/pressentaion/views/widgets/featured_books_list_view.dart';
+import 'package:booklyapp/core/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [CustomAppBar(), FeaturedBooksListView()],
-    );
-  }
-}
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 6),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * .3,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: FeaturedListViewItem(),
-            );
-          },
-        ),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          Gap(49),
+          Text(
+            'Best Saller',
+            style: Styles.titleMedium,
+          )
+        ],
       ),
     );
   }
