d23c8289c65e15d3d75380260695e95de9cd5bc8
SeifRashedd <seifrashedswe@gmail.com>
2024-04-30 14:27:59 +0300

    add text to details book view

--- lib/Features/home/pressentaion/views/widgets/book_details_view_body.dart
+++ lib/Features/home/pressentaion/views/widgets/book_details_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:booklyapp/Features/home/pressentaion/views/widgets/custom_book_details_app_bar.dart';
 import 'package:booklyapp/Features/home/pressentaion/views/widgets/custom_book_image.dart';
+import 'package:booklyapp/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -18,6 +20,22 @@ class BookDetailsViewBody extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(horizontal: width * .20),
             child: const CustomBookImage(),
+          ),
+          const Gap(30),
+          const Text(
+            'The Jungle Book',
+            style: Styles.textStyle30,
+          ),
+          const Gap(6),
+          Opacity(
+            opacity: 0.7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
           ),
         ],
       ),
--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -19,5 +19,5 @@ abstract class Styles {
     fontWeight: FontWeight.w500,
   );
   static const textStyle30 = TextStyle(
-      fontSize: 20, fontWeight: FontWeight.normal, fontFamily: kFontGt);
+      fontSize: 30, fontWeight: FontWeight.normal, fontFamily: kFontGt);
 }
