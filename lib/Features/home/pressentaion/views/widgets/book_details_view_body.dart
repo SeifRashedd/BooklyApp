670a5b505f97cb3757d6fd28735e888acb043296
SeifRashedd <seifrashedswe@gmail.com>
2024-04-30 13:06:47 +0300

    refactor navigation on our app

---
+++ lib/Features/home/pressentaion/views/widgets/book_details_view_body.dart
@@ -0,0 +1,13 @@
+import 'package:booklyapp/Features/home/pressentaion/views/book_details_view.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsViewBody extends StatelessWidget {
+  const BookDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsView(),
+    );
+  }
+}
--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,12 +1,12 @@
-import 'package:booklyapp/Features/home/pressentaion/views/home_view.dart';
 import 'package:booklyapp/Features/splash/presentation/views/widgets/sliding_text_widget.dart';
-import 'package:booklyapp/constants.dart';
+import 'package:booklyapp/core/utils/app_router.dart';
 import 'package:booklyapp/core/utils/assets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
-import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
+
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -28,11 +28,12 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   void navigateToHome() {
     Future.delayed(const Duration(seconds: 2), () {
-      Get.to(
-        () => const HomeView(),
-        transition: Transition.fade,
-        duration: kTransparent,
-      );
+      // Get.to(
+      //   () => const HomeView(),
+      //   transition: Transition.fade,
+      //   duration: kTransparent,
+      // );
+      GoRouter.of(context).push(AppRouter.kHomeView);
     });
   }
 
