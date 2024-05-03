3f449ab72eeefee411e070f66ad984f87f50493b
SeifRashedd <seifrashedswe@gmail.com>
2024-05-03 20:13:20 +0300

    cache network image

--- lib/Features/home/pressentaion/views/widgets/custom_book_image.dart
+++ lib/Features/home/pressentaion/views/widgets/custom_book_image.dart
@@ -1,24 +1,22 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({super.key, required this.imageUrl});
 
-final String imageUrl;
+  final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.5 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: Colors.red,
-          image:  DecorationImage(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(18),
+      child: AspectRatio(
+          aspectRatio: 2.5 / 4,
+          child: CachedNetworkImage(
+            imageUrl: imageUrl,
             fit: BoxFit.fill,
-            image: NetworkImage(imageUrl),
-          ),
-        ),
-      ),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+          )),
     );
   }
 }
