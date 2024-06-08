e765ee43364747d87ccf617de6002cfde202f7cd
SeifRashedd <seifrashedswe@gmail.com>
2024-06-08 16:25:18 +0300

    finish the app

--- lib/Features/home/pressentaion/views/widgets/custom_app_bar.dart
+++ lib/Features/home/pressentaion/views/widgets/custom_app_bar.dart
@@ -1,8 +1,8 @@
 import 'package:booklyapp/core/utils/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+// import 'package:go_router/go_router.dart';
 
-import '../../../../../core/utils/app_router.dart';
+// import '../../../../../core/utils/app_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -13,20 +13,22 @@ class CustomAppBar extends StatelessWidget {
       padding: const EdgeInsets.only(left: 24, right: 24, top: 40, bottom: 20),
       child: Row(
         children: [
-          Image.asset(
-            AssetsData.logo,
-            height: 19,
-          ),
-          const Spacer(),
-          IconButton(
-            onPressed: () {
-              GoRouter.of(context).push(AppRouter.kSearchView);
-            },
-            icon: const Icon(
-              Icons.search,
-              size: 35,
+          Expanded(
+            child: Image.asset(
+              AssetsData.logo,
+              height: 19,
             ),
           ),
+          // const Spacer(),
+          // IconButton(
+          //   onPressed: () {
+          //     GoRouter.of(context).push(AppRouter.kSearchView);
+          //   },
+          //   icon: const Icon(
+          //     Icons.search,
+          //     size: 35,
+          //   ),
+          // ),
         ],
       ),
     );
